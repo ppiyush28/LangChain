96e94e6f33697f50c5f92317295db63e5ba6b4bf
Piyush  Pande <piyushdpande@gmail.com>
2025-08-17 22:06:45 +0530

    HF

--- 2_chatmodel/3_chatmodel_google.py
+++ 2_chatmodel/3_chatmodel_google.py
@@ -6,6 +6,11 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash",
     temperature=0.2,
+    max_tokens=10,
+    #seed=1,
+    top_p=0.9,
+    top_k=40,
+
 )
 
 response = llm.invoke("What is the capital of France?")
---
+++ 2_chatmodel/4_chatmodel_hf.py
@@ -0,0 +1,11 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="openai/gpt-oss-20b", task ="text-generation",)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke("What is the capital of France?")
+print(response)
